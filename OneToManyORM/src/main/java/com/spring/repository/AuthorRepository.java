87a8129dedf0eb72390d3ee1bcfe2ccd0ecd8cab
Sanjana-0110 <150668402+Sanjana-0110@users.noreply.github.com>
2024-05-28 21:18:04 +0530

    Delete OneToManyORM directory

--- OneToManyORM/src/main/java/com/spring/repository/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.model.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
--- OneToManyORM/src/main/java/com/spring/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
