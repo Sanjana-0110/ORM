c505513c6454b7874c5b798d778cdecb56071ad6
Sanjana-0110 <150668402+Sanjana-0110@users.noreply.github.com>
2024-05-30 12:10:34 +0530

    Delete ManyToManyORM directory

--- ManyToManyORM/src/main/java/com/spring/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.model.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-
-}
--- ManyToManyORM/src/main/java/com/spring/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
